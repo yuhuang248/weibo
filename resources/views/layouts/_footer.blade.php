485dc61bf6d0a4659c53bff4a8d2eb03a432a30f
yuhuang248 <584430053@qq.com>
2021-05-09 15:49:58 +0000

    命名路由

--- resources/views/layouts/_footer.blade.php
+++ resources/views/layouts/_footer.blade.php
@@ -3,6 +3,6 @@
   <img class="brand-icon" src="https://cdn.learnku.com/uploads/sites/KDiyAbV0hj1ytHpRTOlVpucbLebonxeX.png" alt="">
   <a href="https://learnku.com/laravel/courses" _blank>刻意练习，每日精进</a>
 <div class="float-right">
-  <a href="/about">关于</a>
+  <a href="{{ route('about') }}">关于</a>
 </div>
 </footer>
